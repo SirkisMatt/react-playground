c9c18218ab3d744cf7039ef8bbc2b0d104bed9ee
Matt Sirkis <sirkis.matt@gmail.com>
2020-09-02 22:28:35 +1200

    added langControls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 //import App from './App';
@@ -6,3 +6,14 @@ import App from './paltrows-power-toes/App';
 import './index.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppLang from './lang-context/AppLang';
+
+ReactDOM.render(
+  <AppLang />,
+  document.getElementById('root')
+);
